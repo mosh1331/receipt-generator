5257ff0494d53f217a991f9fcdc8ac03256692c8
mosh1331 <shamil.tfora@gmail.com>
2025-08-16 15:57:45 +0530

    add prices

--- src/pages/items/Items.jsx
+++ src/pages/items/Items.jsx
@@ -1,57 +1,70 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { addItem,removeItem, resetItems } from "../../redux/slice/itemsSlice";
+import { addItem, removeItem, resetItems } from "../../redux/slice/itemsSlice";
 
 export default function ItemsPage() {
-  const dispatch = useDispatch();
-  const items = useSelector((state) => state.items.list);
-  const [name, setName] = useState("");
+    const dispatch = useDispatch();
+    const items = useSelector((state) => state.items.list);
+    const [name, setName] = useState("");
+    const [price, setPrice] = useState(0);
 
-  const handleAdd = () => {
-    if (!name.trim()) return;
-    dispatch(addItem(name));
-    setName("");
-  };
+    const handleAdd = () => {
+        if (!name.trim() || price === 0) return;
+        const id = `${name}-${price}`
+        dispatch(addItem({id,name,price}));
+        setName("");
+        setPrice(0);
+    };
 
-  return (
-    <div className="bg-white p-4 rounded-xl shadow max-w-md mx-auto">
-      <h2 className="text-xl font-bold mb-4">Items</h2>
-      <div className="flex gap-2 mb-4">
-        <input
-          type="text"
-          placeholder="Item name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          className="border p-2 rounded flex-1"
-        />
-        <button
-          onClick={handleAdd}
-          className="bg-blue-500 text-white px-4 py-2 rounded"
-        >
-          Add
-        </button>
-      </div>
-      <ul className="space-y-2">
-        {items.map((r, idx) => (
-          <li key={idx} className="p-2 border  capitalize rounded flex justify-between">
-            {r}
-            <button
-              onClick={() => dispatch(removeItem(idx))}
-              className="text-red-500"
-            >
-              ✕
-            </button>
-          </li>
-        ))}
-      </ul>
-      {items.length > 0 && (
-        <button
-          onClick={() => dispatch(resetItems())}
-          className="mt-4 bg-gray-500 text-white px-4 py-2 rounded w-full"
-        >
-          Reset All
-        </button>
-      )}
-    </div>
-  );
+    return (
+        <div className="bg-white p-4 rounded-xl shadow max-w-md mx-auto">
+            <h2 className="text-xl font-bold mb-4">Items</h2>
+            <div className=" gap-2 mb-4">
+                <input
+                    type="text"
+                    placeholder="Item name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    className="border p-2 rounded flex-1 w-full"
+                />
+                <div className="flex w-full">
+                    <input
+                        type="number"
+                        min="0"
+                        value={price}
+                        onChange={(e) => setPrice(e.target.value)}
+                        className="border p-2 rounded flex-1"
+                    />
+                    <button
+                        onClick={handleAdd}
+                        className="bg-blue-500 text-white px-4 py-2 rounded"
+                    >
+                        Add
+                    </button>
+                </div>
+            </div>
+            <ul className="space-y-2">
+                {items.map((r, idx) => (
+                    <li key={idx} className="p-2 border  capitalize rounded flex justify-between">
+                        {r.name} -  ₹{r.price}
+                      
+                        <button
+                            onClick={() => dispatch(removeItem(idx))}
+                            className="text-red-500"
+                        >
+                            ✕
+                        </button>
+                    </li>
+                ))}
+            </ul>
+            {items.length > 0 && (
+                <button
+                    onClick={() => dispatch(resetItems())}
+                    className="mt-4 bg-gray-500 text-white px-4 py-2 rounded w-full"
+                >
+                    Reset All
+                </button>
+            )}
+        </div>
+    );
 }
--- src/pages/receiptgenerator/ReceiptGenerator.jsx
+++ src/pages/receiptgenerator/ReceiptGenerator.jsx
@@ -11,7 +11,7 @@ export default function ReceiptGenerator() {
   const recipients = useSelector((state) => state.recipients.list);
   const products = useSelector((state) => state.items.list);
   const [showPreview, setShowPreview] = useState(false)
-
+console.log(products,'producsts')
   const handleItemChange = (index, field, value) => {
     const updated = [...items];
     updated[index][field] = field === "qty" || field === "price" ? Number(value) : value;
@@ -30,12 +30,12 @@ export default function ReceiptGenerator() {
 
   const grandTotal = items.reduce((sum, item) => sum + item.qty * item.price, 0);
 
-//set initial data
+  //set initial data
   useEffect(() => {
-    const initalProduct= products.length > 0 ?  products[0] :''
-    const initalReceivee= recipients.length > 0 ?  recipients[0] :''
+    const initalProduct = products.length > 0 ? products[0] : ''
+    const initalReceivee = recipients.length > 0 ? recipients[0] : ''
     setDate(dayjs().format("YYYY-MM-DD"))
-    setItems([{ description: initalProduct, qty: 1, price: 0 }])
+    setItems([{ description: initalProduct?.name, qty: 1, price: initalProduct?.price }])
     setIssuedTo(initalReceivee)
   }, [])
 
@@ -47,12 +47,12 @@ export default function ReceiptGenerator() {
           type="date"
           value={date}
           onChange={(e) => {
-            console.log(e.target.value,'date')
+            console.log(e.target.value, 'date')
             setDate(e.target.value)
           }}
           className="w-full border p-2 rounded"
         />
-        <select className="capitalize border" name="" value={issuedTo} onChange={(e) => setIssuedTo(e.target.value)} id="">
+        <select className="capitalize border rounded p-2" name="" value={issuedTo} onChange={(e) => setIssuedTo(e.target.value)} id="">
           {recipients.map((r, idx) => (
             <option key={idx} className="p-2 border capitalize rounded flex justify-between">
               {r}
@@ -60,28 +60,62 @@ export default function ReceiptGenerator() {
           ))}
         </select>
         {items.map((item, idx) => (
-          <div key={idx} className="flex gap-2 w-full">
-            <select className="capitalize border" placeholder={'Item'} name="item" value={item.description} onChange={(e) => handleItemChange(idx, "description", e.target.value)} id="">
-              {products.map((r, idx) => (
-                <option key={idx} className="p-2 border capitalize rounded flex justify-between">
-                  {r}
+          <div key={idx} className=" gap-2 w-full">
+            <select
+              className="capitalize border p-2 mb-2 rounded"
+              name="item"
+              value={item.id || ""}
+              onChange={(e) => {
+                const selected = products.find((p) => p.id === e.target.value);
+                handleItemChange(idx, "id", selected.id);
+                handleItemChange(idx, "description", selected.name);
+                handleItemChange(idx, "price", selected.price); // auto-fill price from product
+              }}
+            >
+              <option value="">Select Item</option>
+              {products.map((r) => (
+                <option key={r.id} value={r.id}>
+                  {r.name}
                 </option>
               ))}
             </select>
-            <input
-              type="number"
-              min="1"
-              value={item.qty}
-              onChange={(e) => handleItemChange(idx, "qty", e.target.value)}
-              className="w-[25%] border p-2 rounded"
-            />
-            <input
-              type="number"
-              min="0"
-              value={item.price}
-              onChange={(e) => handleItemChange(idx, "price", e.target.value)}
-              className="w-[25%] border p-2 rounded"
-            />
+            <div className="flex">
+              <div className="flex items-center border rounded w-[70%]">
+                <button
+                  type="button"
+                  onClick={() => handleItemChange(idx, "qty", Math.max(1, item.qty - 1))}
+                  className="px-2 text-lg  text-gray-600 hover:text-black"
+                >
+                  –
+                </button>
+
+                <input
+                  type="number"
+                  min="1"
+                  value={item.qty}
+                  onChange={(e) => handleItemChange(idx, "qty", e.target.value)}
+                  className="w-full text-center p-2 focus:outline-none"
+                  step={1}
+                />
+
+                <button
+                  type="button"
+                  onClick={() => handleItemChange(idx, "qty", Number(item.qty) + 1)}
+                  className="px-2 text-lg text-gray-600 hover:text-black"
+                >
+                  +
+                </button>
+              </div>
+              <div className="w-[30%] border bg-gray-300 p-2 rounded">₹{item.price}</div>
+              {/* <input
+                type="number"
+                min="0"
+                value={item.price}
+                onChange={(e) => handleItemChange(idx, "price", e.target.value)}
+                className="w-[50%] border p-2 rounded"
+                disabled
+              /> */}
+            </div>
           </div>
         ))}
         <button onClick={addItem} className="bg-blue-500 text-white px-4 py-2 rounded">
